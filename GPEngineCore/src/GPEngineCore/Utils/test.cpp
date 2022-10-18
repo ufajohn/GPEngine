2148cc171fc67b18ab4997fe5653321f6fb39de2
John <ufa-john@yandex.ru>
2022-10-18 14:53:23 +0500

    Create test.cpp

---
+++ GPEngineCore/src/GPEngineCore/Utils/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "GPEngineCore/Utils/test.hpp"
+
+namespace GPEngine {
+
+	void SayHello() {
+	
+		std::cout << "Hello from GPEngineCore" << std::endl;
+
+		std::cin.get();
+	}
+}
