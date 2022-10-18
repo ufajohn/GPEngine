55c864898b13f4eb82a2a88fcc7a1b9b748d0fdb
John <ufa-john@yandex.ru>
2022-10-18 14:52:23 +0500

    123

---
+++ GPEngineEditor/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <GPEngineCore/Utils/test.hpp>
+
+int main() {
+	std::cout << "Hello from GPEngineEditor" << std::endl;
+
+	GPEngine::SayHello();
+
+	std::cin.get();
+}
