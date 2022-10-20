ce177f2eb0d8eb80642406870ff0e3db0b93aa99
John <ufa-john@yandex.ru>
2022-10-20 10:20:47 +0500

    123

--- GPEngineCore/Includes/GPEngineCore/Utils/test.hpp
+++
@@ -1,4 +0,0 @@
-namespace GPEngine{
-
-	int CheckGLFW();
-}
--- GPEngineEditor/src/main.cpp
+++ GPEngineEditor/src/main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
-#include <GPEngineCore/Utils/test.hpp>
+#include <memory>
+#include <GPEngineCore/Application.hpp>
 
-int main() {
-	std::cout << "Hello from GPEngineEditor" << std::endl;
+class MyApp : public GPEngine::Application {
+	
+	int frame = 0;
+	
+	virtual void on_update() override {		
+		std::cout << "Update frame: " << ++frame << std::endl;				
+	}
+};
 
-	GPEngine::CheckGLFW();
+int main() {	
+
+	auto myApp = std::make_unique<MyApp>();
+	int returnCode = myApp->start(1024, 768, "MyApp");
+	
 
 	std::cin.get();
 }
